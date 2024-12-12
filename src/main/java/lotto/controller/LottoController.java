409f08fc49fa0c21a3e03033d3b9789842c5557a
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 21:12:58 +0900

    feat: 재시도 로직 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import java.util.function.Supplier;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -14,5 +15,26 @@ public class LottoController {
     }
 
     public void run() {
+    }
+
+    private <T> T rerunTemplate(final Supplier<T> action) {
+        while (true) {
+            try {
+                return action.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void rerunTemplate(final Runnable action) {
+        while (true) {
+            try {
+                action.run();
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
     }
 }
