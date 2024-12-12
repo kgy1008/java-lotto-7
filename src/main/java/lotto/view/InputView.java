d2a4ef7a72574bb373b58c73643310fe4a17dbef
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 21:38:47 +0900

    feat: 로또 번호 리스트로 변환 후 데이터 넘기도록 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,13 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import lotto.common.ErrorMessage;
 
 public class InputView {
+
+    private static final String DELIMITER = ",";
 
     public int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -11,13 +15,20 @@ public class InputView {
         return convertToInt(input);
     }
 
-    public String inputWinningLottoNumber() {
-        return Console.readLine();
+    public List<Integer> inputWinningLottoNumber() {
+        String input = Console.readLine();
+        return parseNumber(input);
     }
 
     public int inputBonusNumber() {
         String input = Console.readLine();
         return convertToInt(input);
+    }
+
+    private List<Integer> parseNumber(final String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(this::convertToInt)
+                .toList();
     }
 
     private int convertToInt(final String input) {
