ced72a9d7214346fd0cbdb74713fdaf8191be90d
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 23:37:11 +0900

    refactor: 불변 리스트로 반환하도록 리팩토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -43,6 +44,6 @@ public class Lotto {
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return Collections.unmodifiableList(numbers);
     }
 }
