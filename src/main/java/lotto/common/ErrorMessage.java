fb483f2a5fcaeb816583213e3c14389cf91acbde
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 21:21:57 +0900

    feat: 사용자로부터 입력받기

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -3,7 +3,9 @@ package lotto.common;
 public enum ErrorMessage {
 
     INVALID_INPUT("아무것도 입력되지 않았습니다. 다시 입력해주세요."),
-    INVALID_INPUT_TYPE("숫자로 입력해주세요.");
+    INVALID_INPUT_TYPE("숫자로 입력해주세요."),
+    INVALID_LOTTO_COUNT("로또 번호는 6개여야 합니다."),
+    INVALID_MONEY("금액이 올바르지 않습니다. 1000원 단위의 금액을 입력해주세요.");
 
     private static final String HEADER = "[ERROR] ";
     private final String message;
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,30 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.common.ErrorMessage;
+
 public class InputView {
+
+    public int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return convertToInt(input);
+    }
+
+    public String inputWinningLottoNumber() {
+        return Console.readLine();
+    }
+
+    public int inputBonusNumber() {
+        String input = Console.readLine();
+        return convertToInt(input);
+    }
+
+    private int convertToInt(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_TYPE.getMessage());
+        }
+    }
 }
